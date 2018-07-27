a42febf69b787e059bc49dbdbc6e5e984c68d999
KABACHIA <kabachia@gmail.com>
2018-07-27 11:58:09 +0300

    Commit to Github

---
+++ desktop/src/com/kabachia/mygdxgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kabachia.mygdxgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kabachia.mygdxgame.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
